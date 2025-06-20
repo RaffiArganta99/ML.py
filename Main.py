daf7929f3562ddf74673ddcf2e53ffa7bfc5c1bc
RaffiArganta99 <e41231765@student.polije.ac.id>
2025-06-21 01:21:49 +0700

    first commit

---
+++ Main.py
@@ -0,0 +1,8 @@
+angka = int(input('masukkan angka: '))
+
+# mengecek input angka dari pengguna
+if angka % 2 == 0:
+    print(f"Angka {angka} adalah GENAP.")
+else:
+    print(f"Angka {angka} adalah GANJIL.")
+    
